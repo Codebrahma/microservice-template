0fc7509e163d71173e0d95d76ccc5c985967afb8
Tarun Batra <tarun.batra00@gmail.com>
2017-02-03 19:44:23 +0530

    Adds sample publish and rpc call

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var app = require('./app');
 var logger = require('./app/logger');
-
+var bus = require('./app/bus');
 app();
+
+// Sample publish
+bus.publish('sample.message',   // Event type
+  { desiredProperty: true },    // Actual message
+  { ack: true }                 // Additional options
+);
+
+// Sample RPC request
+bus.rpc.call('sample.request',  // RPC type
+  { desiredProperty: true },    // RPC params
+  function (reply) {            // Callback function with reply
+    logger.info(reply);
+  }
+);
 
 logger.info('***** Microservice template started *****');
